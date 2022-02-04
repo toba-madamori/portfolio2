0469175c53eb3876394741781c689f9adef652b2
Toba Madamori <tobajamezmadamori@gmail.comgit config --global user.name Toba>
2022-02-04 16:59:24 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv')
+const express = require('express')
+const app = express()
+
+// view engine
+app.set('view engine', 'ejs')
+
+// static files
+app.use(express.static('public'))
+
+// routes
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, ()=>{
+    console.log(`server is listening on port ${port}....`);
+})
+
